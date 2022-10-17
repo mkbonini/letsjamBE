60a8b592d03f9ab87a5b69d7f8901449febc157e
Cory Bethune <corybbethune@gmail.com>
2022-10-17 15:51:08 -0700

    Create database. User show endpoint works!"

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,17 @@
 
 from flask import Flask
 import os
+from flask import request
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config[‘SQLALCHEMY_DATABASE_URI’] = ‘sqlite:///’+ \
-                os.path.join(basedir, ‘db.sqlite3’)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ \
+                os.path.join(basedir, 'db.sqlite3')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -33,7 +36,7 @@ class User(db.Model):
 
 class Instrument(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string)
+    name = db.Column(db.String)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self, name):
@@ -45,16 +48,30 @@ user_instrument = db.Table('user_instrument',
     db.Column('plays_or_needs', db.Integer)
 )
 
-
-
-
-
-
-
-
-class NoteSchema(ma.SQLAlchemyAutoSchema):
+class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = NoteModel
+        model = User
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+@app.route('/users/<int:user_id>/')
+def show_user(user_id):
+    user = User.query.get(user_id)
+    return user_schema.jsonify(user)
+
+@app.route('/users/', methods=['POST'])
+def create_user():
+    name = request.json.get('name', '')
+    display_email = request.json.get('display_email', '')
+    picture_url = request.json.get('picture_url', '')
+    about = request.json.get('about', '')
+    zipcode = request.json.get('zipcode', '')
+    user = User(name=name, display_email=display_email, picture_url=picture_url, about=about, zipcode=zipcode)
+
+    db.session.add(user)
+    db.session.commit()
+    return user_schema.jsonify(user)
 
 
 

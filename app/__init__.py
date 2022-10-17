563f88ea32e9b2c30b9433ef725a9c15ac0de680
Cory Bethune <corybbethune@gmail.com>
2022-10-17 14:18:05 -0700

    Add requirements. Can now run the server locally.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-# app/__init__.py: Flask application instance 
+# app/__init__.py: Flask application instance
 
-from flask import Flask 
+from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
 
 from app import routes

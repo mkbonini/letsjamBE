608554a677828487cd76282aba7422047a1102fc
mkbonini <mkbonini@gmail.com>
2022-10-22 16:16:02 -0600

    user endpoint schema update.
    Co-authored-by: Gwendolyn <gwen_marina@yahoo.com>
    Co-authored-by: Jared <jaredhardinger@gmail.com>
    Co-authored-by: Cory <corybbethune@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ from marshmallow_jsonapi import fields, Schema
 
 
 app = Flask(__name__)
+app.config ['JSON_SORT_KEYS'] = False
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_uri = "postgresql://mgdeiuhjogmlzz:0a44187da32e2c27267fa407dbd2e9370d19c5c00137900902273076c9f27737@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d9e64hhn0dh9ou"
@@ -118,26 +119,28 @@ class UserSchema(Schema):
     zipcode = fields.Str()
     picture_url = fields.Str()
     instruments = fields.Nested(lambda: InstrumentSchema(only=("name","id"), many=True))
-    # instruments = fields.Relationship(
-    #     related_url = "/users/(user_id)/instruments",
-    #     related_url_kwargs = {"user_id": "<id>"},
-    #     many=True,
-    #     include_resource_linkage=True,
-    #     type_ = "instruments",
-    #     schema = "InstrumentSchema",
-    # )
+    needs_instruments = fields.Nested(lambda: NeedsInstrumentSchema(only=("name","id"), many=True))
+    genres = fields.Nested(lambda: GenreSchema(only=("name","id"), many=True))
 
     class Meta:
         # model = User
         type_ = "user"
         strict = True
+        ordered = True
 
-class InstrumentSchema(Schema):
+class InstrumentSchema(ma.SQLAlchemyAutoSchema):
     id = fields.Str(dump_only=True)
     name = fields.Str()
     class Meta:
         # model = Instrument
         type_ = "instrument"
+
+class NeedsInstrumentSchema(ma.SQLAlchemyAutoSchema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str()
+    class Meta:
+        # model = Instrument
+        type_ = "needs_instrument"
 
 instrument_schema = InstrumentSchema()
 instruments_schema = InstrumentSchema(many=True)
@@ -150,21 +153,50 @@ class UserInstrumentSchema(ma.SQLAlchemyAutoSchema):
 user_instrument_schema = UserInstrumentSchema()
 
 class GenreSchema(ma.SQLAlchemyAutoSchema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str()
+
     class Meta:
         model = Genre
 
 genre_schema = GenreSchema()
 genres_schema = GenreSchema(many=True)
 
+class UserConnectionsSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str()
+    class Meta:
+        model = Genre
 
-@app.route('/users/<int:user_id>/')
+@app.route('/api/v1/users/<int:user_id>/')
 def show_user(user_id):
-    user = db.session.get(User, user_id)
     # breakpoint()
+    user = db.session.get(User, user_id)
     return UserSchema().dump(user)
     # return user_schema.jsonify(user)
 
-@app.route('/users/', methods=['POST'])
+@app.route('/api/v1/users/<int:user_id>/connections')
+def show_user_connections(user_id):
+    user = db.session.get(User, user_id)
+    pending_connections = []
+    connections = []
+    pending_requests = []
+    connection_list = session.query(user_connection).filter_by(status = 'PENDING', user_id = user.id).all()
+    for conns in connection_list:
+        pending_connections.append( session.query(User).filter_by(id=conns.friend_id).all()[0] )
+    connection_list = session.query(user_connection).filter_by(status = 'PENDING', friend_id = user.id).all()
+    for conns in connection_list:
+        pending_requests.append( session.query(User).filter_by(id=conns.user_id).all()[0] )
+    connection_list = session.query(user_connection).filter_by(status = 'APPROVED', user_id = user.id).all()
+    connection_list = connection_list + session.query(user_connection).filter_by(status = 'APPROVED', friend_id = user.id).all() 
+    for conns in connection_list:
+        connections.append( session.query(User).filter_by(id=conns.friend_id).all()[0] )
+
+
+    return UserSchema().dump(user)
+    # return user_schema.jsonify(user)
+
+@app.route('/api/v1/users/', methods=['POST'])
 def create_user():
     name = request.json.get('name', '')
     display_email = request.json.get('display_email', '')
@@ -174,9 +206,9 @@ def create_user():
     user = User(name=name, display_email=display_email, picture_url=picture_url, about=about, zipcode=zipcode)
     db.session.add(user)
     db.session.commit()
-    return user_schema.jsonify(user)
+    return UserSchema().dump(user)
 
-@app.route('/instruments/', methods=['POST'])
+@app.route('/api/v1/instruments/', methods=['POST'])
 def create_instrument():
     name = request.json.get('name', '')
     instrument = Instrument(name=name)
@@ -186,7 +218,7 @@ def create_instrument():
     db.session.commit()
     return instrument_schema.jsonify(instrument)
 
-@app.route('/genres/', methods=['POST'])
+@app.route('/api/v1/genres/', methods=['POST'])
 def create_genre():
     name = request.json.get('name', '')
     genre = Genre(name=name)
@@ -195,21 +227,46 @@ def create_genre():
     db.session.commit()
     return genre_schema.jsonify(genre)
 
-@app.route('/users/<int:user_id>/instruments/<int:instrument_id>', methods=['POST'])
+@app.route('/api/v1/users/<int:user_id>/instruments/<int:instrument_id>', methods=['POST'])
 def create_user_instrument(user_id, instrument_id):
     ins = user_instrument.insert().values(user_id=user_id, instrument_id=instrument_id)
     db.engine.execute(ins)
     db.session.commit()
-    return user_instrument_schema.jsonify(ins)
+    return "connection added"
 
-@app.route('/users/<int:user_id>/needs_instruments/<int:instrument_id>', methods=['POST'])
-def create_user_needs_instrument(user_id, instrument_id):
-    ins = user_needs_instrument.insert().values(user_id=user_id, instrument_id=instrument_id)
+@app.route('/api/v1/users/<int:user_id>/needs_instruments/<int:needs_instrument_id>', methods=['POST'])
+def create_user_needs_instrument(user_id, needs_instrument_id):
+    ins = user_needs_instrument.insert().values(user_id=user_id, needs_instrument_id=needs_instrument_id)
     db.engine.execute(ins)
     db.session.commit()
-    return user_instrument_schema.jsonify(ins)
+    return "connection added"
 
-@app.route('/users/<int:user_id>/instruments', methods=['GET'])
+@app.route('/api/v1/users/<int:user_id>/genres/<int:genre_id>', methods=['POST'])
+def create_user_genre(user_id, genre_id):
+    ins = user_genre.insert().values(user_id=user_id, genre_id=genre_id)
+    db.engine.execute(ins)
+    db.session.commit()
+    return "connection added"
+
+@app.route('/api/v1/users/<int:user_id>/connections/<int:friend_id>', methods=['POST'])
+def create_user_connection(user_id, friend_id):
+    ins = user_connection.insert().values(user_id=user_id, friend_id=friend_id, status='PENDING')
+    db.engine.execute(ins)
+    db.session.commit()
+    return "connection added"
+
+@app.route('/api/v1/users/<int:user_id>/connections/<int:friend_id>', methods=['PATCH'])
+def update_user_connection(user_id, friend_id):
+    status_input = request.json.get('status', '')
+    connection = session.query(user_connection).filter_by(user_id = user_id, friend_id = friend_id).all()[0] 
+    # connection.update({connection.status: status_input})
+    user_connection.update().values(status=status_input).where(user_connection.c.user_id==user_id).where( user_connection.c.friend_id==friend_id    )
+    # ins = user_connection.update().values(user_id=user_id, friend_id=friend_id, status=status)
+    # db.engine.execute(ins)
+    db.session.commit()
+    return "connection updated"
+
+@app.route('/api/v1/users/<int:user_id>/instruments', methods=['GET'])
 def get_user_instruments(user_id):
     user = db.session.get(User, user_id)
     return InstrumentSchema(many=True).dump(user.instruments)

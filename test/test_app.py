91efb93c108df697f8cb3cdb7b548753cf072a68
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-10-23 15:16:09 -0700

    test needs instrument

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,4 @@
-from app import app, User, Instrument
+from app import app, User, Instrument, NeedsInstrument, Genre
 import pytest
 
 def test_always_passes():
@@ -16,4 +16,21 @@ def test_new_user():
 def test_new_instrument():
     instrument = Instrument('Clarinet')
     assert instrument.name == 'Clarinet'
-   
+
+
+def test_needs_instrument():
+    needs_instrument = NeedsInstrument('Piano')
+    assert needs_instrument.name == 'Piano'
+
+# class NeedsInstrument(db.Model):
+#     __tablename__ = "needs_instrument"
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+#    class Instrument(db.Model):
+#     __tablename__ = "instrument"
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+

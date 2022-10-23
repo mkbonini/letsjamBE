7825d8c3551af3b42e4dabbd36973313dbd96acb
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-10-23 15:10:11 -0700

    test new user

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, Session, sessionmaker
 from marshmallow_jsonapi import fields, Schema
 
-
 app = Flask(__name__)
 app.config ['JSON_SORT_KEYS'] = False
 
--- test/test_app.py
+++ test/test_app.py
@@ -1,2 +1,14 @@
+from app import app, User
+import pytest
+
 def test_always_passes():
     assert True
+
+def test_new_user():
+    user = User('Bory Cethune', 'bcethune@email.com', 'www.pictureofmyself.com', 'I want to jam!', '80000')
+    assert user.name == 'Bory Cethune'
+    assert user.display_email == 'bcethune@email.com'
+    assert user.picture_url == 'www.pictureofmyself.com'
+    assert user.about == 'I want to jam!'
+    assert user.zipcode == '80000'
+

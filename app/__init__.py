1816cd6c2cfeedb44ed5d663710fecc3660e319f
mkbonini <mkbonini@gmail.com>
2023-01-04 17:09:41 -0700

    Fly Deployment

--- app/__init__.py
+++ app/__init__.py
@@ -18,9 +18,10 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config ['JSON_SORT_KEYS'] = False
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_uri = "postgresql://mgdeiuhjogmlzz:0a44187da32e2c27267fa407dbd2e9370d19c5c00137900902273076c9f27737@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d9e64hhn0dh9ou"
+db_uri = "postgres://postgres:2npp51WDpjnxMdi@lets-jam-db.internal:5432"
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
